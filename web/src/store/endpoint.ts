aad5658d7da45865a172882ceae92687f9bfb31f
jr-MS <81400625+jr-MS@user.noreply.github.com>
2025-04-18 03:57:57 +0800

    update web endpoint

--- web/src/store/endpoint.ts
+++ web/src/store/endpoint.ts
@@ -1,7 +1,7 @@
 
 export const WS_ENDPOINT = "wss://contoso-smart-home-demo.politemeadow-b5ff133c.eastasia.azurecontainerapps.io";
 export const API_ENDPOINT = "https://contoso-smart-home-demo.politemeadow-b5ff133c.eastasia.azurecontainerapps.io";
-// export const WEB_ENDPOINT = "https://contoso-smart-home-frontend.politemeadow-b5ff133c.eastasia.azurecontainerapps.io";
+export const WEB_ENDPOINT = "https://wonderful-bay-0b065731e.6.azurestaticapps.net/";
 
 // export const WS_ENDPOINT = "ws://localhost:8000";
 // export const API_ENDPOINT = "http://localhost:8000";
